c408cc629318eb93bf61242d231dd2d85df3b32e
Kuma <michael-naef@hotmail.com>
2019-05-31 00:59:45 +0200

    fixed some flickering

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -33,12 +33,11 @@ export class TransactionList extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        if(prevProps === this.props){
-            return;
+        if(prevProps !== this.props){
+            getTransactions(this.props.token, this.props.fromDate, this.props.toDate, this.props.count, this.props.skip)
+                .then(returnedTransactions => this.setState({transactions: returnedTransactions.result}))
         }
-        console.log('update of transactionList');
-        getTransactions(this.props.token, this.props.fromDate, this.props.toDate, this.props.count, this.props.skip)
-            .then(returnedTransactions => this.setState({transactions: returnedTransactions.result}))
+
     }
 
     render = () => <TransactionsToList transactions={this.state.transactions} showDate={this.props.showDate}/>
